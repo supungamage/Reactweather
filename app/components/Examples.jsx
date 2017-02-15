f1e9c597afbdbb01b1bb7c1bbe6a673b54c3a747
supun <supun.gamage@ebuilder.com>
2017-02-15 19:04:10 +0530

    Example page changers

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return(
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to Example page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are few example location to try...</p>
+      <ol>
+        <li>
+          <Link to='/?location=Colombo'>Colombo, SL</Link>
+        </li>
+        <li>
+          <Link to='/?location=Mumbai'>Mumbai, India</Link>
+        </li>
+      </ol>
     </div>
   )
 };
