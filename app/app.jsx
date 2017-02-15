1cbc5fe878c8b269713e3e07e11f4be0d8c2ac4e
supun <supun.gamage@ebuilder.com>
2017-02-15 17:50:28 +0530

    added foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,9 @@ var Wheather = require('Wheather');
 var About = require('About');
 var Examples = require('Examples');
 
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
